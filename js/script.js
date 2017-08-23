40ed49f6a761c298b0d5252fb585fcee594c2a74
Tim Stannard <stannard.tim@gmail.com>
2017-08-24 11:36:18 +1200

    markers have fb id on click

--- js/script.js
+++ js/script.js
@@ -2,45 +2,44 @@ var map, marker, infobox;
 var markers = [];
 var markerLocations = [
 
-{"Place":"Lorettas","FB ID":"4.07703E+14","Lat":-41.2943592,"Lng":174.7753067},
-{"Place":"Five Boroughs","FB ID":"fiveboroughsnz","Lat":-41.294281,"Lng":174.784976},
-{"Place":"Sweet mothers","FB ID":"1.96715E+14","Lat":-41.2942663,"Lng":174.7832352},
-{"Place":"Dragons","FB ID":"WellingtonDragons","Lat":-41.2930319,"Lng":174.7815826},
-{"Place":"Ortega","FB ID":"1.71329E+14","Lat":-41.2943537,"Lng":174.7848696},
-{"Place":"Nandos","FB ID":"Nandos","Lat":-41.2926429,"Lng":174.7794958},
-{"Place":"Big Thumb","FB ID":"BigThumbChineseRestaurant","Lat":-41.2929024,"Lng":174.7826877},
-{"Place":"Crab Shack","FB ID":"CrabShackWGN","Lat":-41.2846651,"Lng":174.7791097},
-{"Place":"Valentines","FB ID":"CLOSED","Lat":-41.2242074,"Lng":174.8743884},
-{"Place":"Dynasty","FB ID":"","Lat":-41.134147,"Lng":174.83931},
-{"Place":"Burger King","FB ID":"BurgerKingNZ","Lat":-41.291003,"Lng":174.776764},
-{"Place":"Mekong Cafe","FB ID":"MekongcafeNZ","Lat":-41.294934,"Lng":174.775795},
-{"Place":"The Oaks","FB ID":"1.51125E+11","Lat":-41.2916393,"Lng":174.7767167},
-{"Place":"Viva Mexico","FB ID":"1.85318E+14","Lat":-41.3128611,"Lng":174.7797865},
-{"Place":"Olive Cafe","FB ID":"4.10321E+14","Lat":-41.2943711,"Lng":174.7749263},
-{"Place":"Abrakebabra","FB ID":"kebabcentral.co.nz","Lat":-41.2909774,"Lng":174.7764203},
-{"Place":"Little India","FB ID":"littleindiaonline","Lat":-41.292631,"Lng":174.77614},
-{"Place":"Customs Brew Bar","FB ID":"coffeesupremenz","Lat":-41.29358,"Lng":174.77621},
-{"Place":"Six Barrel Cafe","FB ID":"sixbarrelsodaco","Lat":-41.2921256,"Lng":174.7774789},
-{"Place":"Husk","FB ID":"huskbar","Lat":-41.293014,"Lng":174.775106},
-{"Place":"Puro Chile","FB ID":"purochilewellington","Lat":-41.2867563,"Lng":174.775872},
-{"Place":"Cicio Cacio","FB ID":"osteriaciciocacio","Lat":-41.313102,"Lng":174.779933},
-{"Place":"Enigma","FB ID":"EnigmaCafeNZ","Lat":-41.2924698,"Lng":174.7793674},
-{"Place":"Joe's Garage","FB ID":"JoesGarageSumner","Lat":-41.2924802,"Lng":174.7818218},
-{"Place":"Prefab","FB ID":"PrefabEatery","Lat":-41.2955892,"Lng":174.7784688},
-{"Place":"Lone Star","FB ID":"LoneStarWaitakere","Lat":-41.294375,"Lng":174.780545},
-{"Place":"Monsoon Poon","FB ID":"monsoonpoon","Lat":-41.2930769,"Lng":174.7831731},
-{"Place":"Beach Babylon","FB ID":"beachbabylon","Lat":-41.291438,"Lng":174.7941375},
-{"Place":"Kilim","FB ID":"KilimCafePetone","Lat":-41.2239984,"Lng":174.8745722},
-{"Place":"Buddha Stix","FB ID":"2.97548E+14","Lat":-41.2272323,"Lng":174.8841316},
-{"Place":"Aroy","FB ID":"TakaAroyDee","Lat":-41.2923466,"Lng":174.7762906},
-{"Place":"Forage","FB ID":"","Lat":-41.2936859,"Lng":174.7696188},
-{"Place":"Subway","FB ID":"subway","Lat":-41.2842567,"Lng":174.7749646},
-{"Place":"Indian Taj","FB ID":"4.14832E+14","Lat":-41.2904399,"Lng":174.7741311},
-{"Place":"Grill Meats Beer","FB ID":"grillmeatsbeer.co.nz","Lat":-41.295561,"Lng":174.774516},
-{"Place":"Portlander","FB ID":"PortlanderBarAndGrill","Lat":-41.281023,"Lng":174.778522},
-{"Place":"El Matador","FB ID":"ElMatadorWellington","Lat":-41.294958,"Lng":174.7746409},
-{"Place":"Ekim Burger","FB ID":"2.01864E+14","Lat":-41.2961785,"Lng":174.7742578},
-{"Place":"Counter Culture","FB ID":"countercultureboardgamecafe","Lat":-41.2944616,"Lng":174.7729038}
+{'Place':'Lorettas','FBID':'407702692702904','Lat':-41.2943592,'Lng':174.7753067},
+{'Place':'Five Boroughs','FBID':'fiveboroughsnz','Lat':-41.294281,'Lng':174.784976},
+{'Place':'Sweet mothers','FBID':'196714513687573','Lat':-41.2942663,'Lng':174.7832352},
+{'Place':'Dragons','FBID':'WellingtonDragons','Lat':-41.2930319,'Lng':174.7815826},
+{'Place':'Ortega','FBID':'171328776240092','Lat':-41.2943537,'Lng':174.7848696},
+{'Place':'Nandos','FBID':'Nandos','Lat':-41.2926429,'Lng':174.7794958},
+{'Place':'Big Thumb','FBID':'BigThumbChineseRestaurant','Lat':-41.2929024,'Lng':174.7826877},
+{'Place':'Crab Shack','FBID':'CrabShackWGN','Lat':-41.2846651,'Lng':174.7791097},
+{'Place':'Dynasty','FBID':'','Lat':-41.134147,'Lng':174.83931},
+{'Place':'Burger King','FBID':'BurgerKingNZ','Lat':-41.291003,'Lng':174.776764},
+{'Place':'Mekong Cafe','FBID':'MekongcafeNZ','Lat':-41.294934,'Lng':174.775795},
+{'Place':'The Oaks','FBID':'151124838097','Lat':-41.2916393,'Lng':174.7767167},
+{'Place':'Viva Mexico','FBID':'185318438283867','Lat':-41.3128611,'Lng':174.7797865},
+{'Place':'Olive Cafe','FBID':'410321435844094','Lat':-41.2943711,'Lng':174.7749263},
+{'Place':'Abrakebabra','FBID':'kebabcentral.co.nz','Lat':-41.2909774,'Lng':174.7764203},
+{'Place':'Little India','FBID':'littleindiaonline','Lat':-41.292631,'Lng':174.77614},
+{'Place':'Customs Brew Bar','FBID':'coffeesupremenz','Lat':-41.29358,'Lng':174.77621},
+{'Place':'Six Barrel Cafe','FBID':'sixbarrelsodaco','Lat':-41.2921256,'Lng':174.7774789},
+{'Place':'Husk','FBID':'huskbar','Lat':-41.293014,'Lng':174.775106},
+{'Place':'Puro Chile','FBID':'purochilewellington','Lat':-41.2867563,'Lng':174.775872},
+{'Place':'Cicio Cacio','FBID':'osteriaciciocacio','Lat':-41.313102,'Lng':174.779933},
+{'Place':'Enigma','FBID':'EnigmaCafeNZ','Lat':-41.2924698,'Lng':174.7793674},
+{'Place':'Joe\'s Garage','FBID':'JoesGarageSumner','Lat':-41.2924802,'Lng':174.7818218},
+{'Place':'Prefab','FBID':'PrefabEatery','Lat':-41.2955892,'Lng':174.7784688},
+{'Place':'Lone Star','FBID':'LoneStarWaitakere','Lat':-41.294375,'Lng':174.780545},
+{'Place':'Monsoon Poon','FBID':'monsoonpoon','Lat':-41.2930769,'Lng':174.7831731},
+{'Place':'Beach Babylon','FBID':'beachbabylon','Lat':-41.291438,'Lng':174.7941375},
+{'Place':'Kilim','FBID':'KilimCafePetone','Lat':-41.2239984,'Lng':174.8745722},
+{'Place':'Buddha Stix','FBID':'297547646942699','Lat':-41.2272323,'Lng':174.8841316},
+{'Place':'Aroy','FBID':'TakaAroyDee','Lat':-41.2923466,'Lng':174.7762906},
+{'Place':'Forage','FBID':'','Lat':-41.2936859,'Lng':174.7696188},
+{'Place':'Subway','FBID':'subway','Lat':-41.2842567,'Lng':174.7749646},
+{'Place':'Indian Taj','FBID':'414831955313156','Lat':-41.2904399,'Lng':174.7741311},
+{'Place':'Grill Meats Beer','FBID':'grillmeatsbeer.co.nz','Lat':-41.295561,'Lng':174.774516},
+{'Place':'Portlander','FBID':'PortlanderBarAndGrill','Lat':-41.281023,'Lng':174.778522},
+{'Place':'El Matador','FBID':'ElMatadorWellington','Lat':-41.294958,'Lng':174.7746409},
+{'Place':'Ekim Burger','FBID':'201864206519644','Lat':-41.2961785,'Lng':174.7742578},
+{'Place':'Counter Culture','FBID':'countercultureboardgamecafe','Lat':-41.2944616,'Lng':174.7729038}
 
 ];
 
@@ -56,10 +55,10 @@ function init(){
 				disableDefaultUI: false, //turn off user interface
 				scrollwheel: true,
 				draggable: true,
-				draggableCursor: "pointer",
-				draggingCursor: "crosshair",
+				draggableCursor: 'pointer',
+				draggingCursor: 'crosshair',
 				fullscreenControl: true,
-				backgroundColor: "grey",
+				backgroundColor: 'grey',
 				ketboardShortcuts: false,
 				mapTypeControlOptions: {
 						position: google.maps.ControlPosition.TOP_CENTER
@@ -70,32 +69,32 @@ function init(){
 								{elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
 						{
 								stylers:[
-								{ hue: "#556b2f" },
+								{ hue: '#556b2f' },
 								{ saturation: -20 }
 								]
 						}, 
 						{
-								featureType: "road",
-								elementType: "geometry",
+								featureType: 'road',
+								elementType: 'geometry',
 								stylers: [
-										{  hue: "#556b2f" },
+										{  hue: '#556b2f' },
 										{ lightness: 0 },
-										{ visibility: "simplified" }
+										{ visibility: 'simplified' }
 								]
 
 						},
 						{
-								featureType: "transit",
-								elementType: "labels",
+								featureType: 'transit',
+								elementType: 'labels',
 								stylers: [
-										{hue: "#ff0066"},
+										{hue: '#ff0066'},
 										{saturation: +80}
 								]
 						},
 						{
-								featureType: "water",
+								featureType: 'water',
 								stylers: [
-										{  hue: "#3D5A6C" },
+										{  hue: '#3D5A6C' },
 										{ lightness: 0 }
 								]
 										
@@ -193,7 +192,7 @@ function init(){
 				]
 		}
 
-	map = new google.maps.Map(document.getElementById("map"), mapOptions)
+	map = new google.maps.Map(document.getElementById('map'), mapOptions)
 	addAllMarkers();
 
 	};
@@ -204,16 +203,22 @@ google.maps.event.addDomListener(window, 'load', init);
 function addAllMarkers(){
 	for (var i = 0; i < markerLocations.length; i++) {
 		marker = new google.maps.Marker({
-			position:{
+			position: {
 				lat: markerLocations[i].Lat,
 				lng: markerLocations[i].Lng
 			},
 			map: map,
-			animation: google.maps.Animation.DROP
-			// icon: "image/icon.png",
-
-		})
+			animation: google.maps.Animation.DROP,
+			icon: 'img/icon.png',
+			fb_id: markerLocations[i].FBID,
+			i: i
+		});
 		markers.push(marker);
 
+		google.maps.event.addListener(marker, "click", function(){
+			console.log(this.fb_id);
+		});
 	};
 }
+
+// showPopup(fb_id)
